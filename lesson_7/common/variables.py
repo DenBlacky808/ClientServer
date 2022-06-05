af9ee7da93de4e16e825a2184f3f22c86f3a4977
Denis Chernyavskiy <247300@gmail.com>
2022-06-05 23:15:34 +0300

    lesson_7

---
+++ lesson_7/common/variables.py
@@ -0,0 +1,20 @@
+import logging
+
+port = 7777
+ip_address = '127.0.0.1'
+max_connect = 5
+max_pack_length = 1024
+encoding = 'utf-8'
+logging_level = logging.DEBUG
+
+action = 'action'
+time_var = 'time'
+user = 'user'
+acc_name = 'acc_name'
+sender = 'sender'
+
+presence = 'presence'
+response = 'response'
+error = 'error'
+MESSAGE = 'message'
+MESSAGE_TEXT = 'mess_text'
